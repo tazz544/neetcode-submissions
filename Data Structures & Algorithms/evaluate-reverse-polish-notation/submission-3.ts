38a3db9403e3f256b5b62a2fe46ef58ea44a84ad
Krzysztof Drozdowski <k.drozdowskijg@gmail.com>
2026-08-11 13:49:06 +0200

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens: string[]): number {
+        let r = 0;
+        let s = [];
+
+        const _updateVal = (op: (acc: number, val: number) => number) => {
+            r = s.reduce(op, r);
+            s = [r];
+        }
+
+        for (const t of tokens) {
+            if (!Number.isNaN(Number(t))) s.push(Number(t));
+            if (t === '+') {
+                _updateVal((acc, val) => acc + val)
+                continue;
+            }
+            if (t === '-') {
+                _updateVal((acc, val) => acc - val)
+                continue;
+            }
+            if (t === '*') {
+                _updateVal((acc, val) => acc * val)
+                continue;
+            }
+            if (t === '/') {
+                _updateVal((acc, val) => acc / val)
+                continue;
+            }
+        }
+
+        return r;
+    }
+}
